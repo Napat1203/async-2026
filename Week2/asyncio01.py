58fd70e64f1328878fd6f9218dc21736bb256895
Napat <preem07188@gmail.com>
2026-07-01 10:36:58 +0700

    <python>asyncioInClass

--- Week2/asyncio01.py
+++ Week2/asyncio01.py
@@ -1,2 +1,8 @@
 # Program 1: The First Coroutine Function
 # Concept: Understanding async def and how it differs from a normal function.
+import asyncio 
+
+async def greet():
+    print("Hello from coroutine!")
+
+print(type(greet))  # This will show that greet is a coroutine function, not a normal function.
