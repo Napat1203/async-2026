2d3332e2c9dff3900e7b5e8dca055e2e9cd763db
Napat <preem07188@gmail.com>
2026-07-01 11:45:57 +0700

    <python>3file

--- Week2/restaurant_01_multiprocess.py
+++ Week2/restaurant_01_multiprocess.py
@@ -1,45 +1,42 @@
-import asyncio 
+from time import sleep, ctime, time
 import multiprocessing
-from time import time, ctime 
 
-async def greet_diners(customer):
-    print(f"{ctime()} Greeting for Customer-{customer} ...")
-    await asyncio.sleep(1)
-    print(f"{ctime()} Greeting for Customer-{customer} ...Done!") 
+def greet_diners(customer):
+    print(f"{ctime()} -> Greeting for customer-{customer} ...")
+    sleep(1)
+    print(f"{ctime()} -> Greeting for customer-{customer} ...Done!")
 
-async def customer_workflow(customer):
-    print(f"{ctime()} Taking order for Customer-{customer} ...")
-    await asyncio.sleep(1)
-    print(f"{ctime()} Taking order for Customer-{customer} ...Done!") 
+def customer_private_workflow(customer):
+    print(f"{ctime()} [Process-{customer}] -> Taking Order ...")
+    sleep(1)
+    print(f"{ctime()} [Process-{customer}] -> Taking Order ...Done!")
 
-    print(f"{ctime()} Cooking for Customer-{customer} ...")
-    await asyncio. sleep(1)
-    print(f"{ctime()} Cooking for Customer-{customer} ...Done!") 
+    print(f"{ctime()} [Process-{customer}] -> Cooking ...")
+    sleep(1)
+    print(f"{ctime()} [Process-{customer}] -> Cooking ...Done!")
 
-    print(f"{ctime()} Mini Bar for Customer-{customer} ...")
-    await asyncio.sleep(1)
-    print(f"{ctime()} Mini Bar for Customer-{customer} ...Done!")      
+    print(f"{ctime()} [Process-{customer}] -> Mini bar ...")
+    sleep(1)
+    print(f"{ctime()} [Process-{customer}] -> Mini bar ...Done!")
 
-    print(f"\n{ctime()} [Task - {customer}] All served!\n")
-async def main():
+if __name__ == "__main__":
     customers = ["A", "B", "C"]
 
     start_time = time()
-    for customer in customers:
-        await greet_diners(customer)
-         
-    print(f"{ctime()} ---All customers greeted. FORKING into indepentent Tasks (Brendon)---\n")
 
-    tasks = []
     for customer in customers:
-        task = asyncio.create_task(customer_workflow(customer))
-        tasks.append(task)
-
-    for task in tasks:
-        await task
+        greet_diners(customer)
     
-    duration = time() - start_time
-    print(f"Finished Entire Restaurant Operation in {duration:.2f} seconds ") 
+    print(f"{ctime()} --- All customers greeted, FORKING into independent processes for each customer ...")
 
-if __name__ == "__main__":
-    asyncio.run(main())
+    processes = []
+
+    for customer in customers:
+        p = multiprocessing.Process(target=customer_private_workflow, args=(customer,))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+    print(f"Total Operation time: {time() - start_time:.2f} seconds")
