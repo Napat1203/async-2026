03d5655d9611b670c9e2733573888a7aedd2c646
Napat <preem07188@gmail.com>
2026-08-05 12:01:32 +0700

    test

--- Week6/robots.py
+++ Week6/robots.py
@@ -5,12 +5,12 @@ import httpx
 # ==========================================
 # 1. Configuration & Constants
 # ==========================================
-STUDENT_ID = "" 
-BASE_URL = ""
+STUDENT_ID = "6710301034" 
+BASE_URL = "http://172.16.2.117:8088"
 
 # กำหนดลำดับชิ้นส่วนและหุ่นยนต์
 PARTS = ["A", "B", "C"]
-ROBOTS = ["robot_1", "robot_2", "robot_3"]
+ROBOTS = ["robot_1", "robot_2", "robot_3", "robot_4"]
 
 # ==========================================
 # 2. Async Functions Development
@@ -18,17 +18,25 @@ ROBOTS = ["robot_1", "robot_2", "robot_3"]
 
 async def reset_factory(client: httpx.AsyncClient):
     """ส่ง Request เพื่อทำการ Reset สถานะของหุ่นยนต์ทั้งหมดของรหัสนักเรียนนี้"""
+    url = f"{BASE_URL}/student/{STUDENT_ID}/reset"
+    response = await client.post(url, timeout=5.0) 
+    data = response.json()
+    return f"[{data}]"
     # TODO: เติมโค้ดการส่ง POST request ไปยัง /student/{STUDENT_ID}/reset
-    pass
+
 
 async def grab_part(client: httpx.AsyncClient, robot_id: str, part: str):
     """สั่งให้หุ่นยนต์หยิบชิ้นส่วน 1 ชิ้น"""
+    url = f"{BASE_URL}/student/{STUDENT_ID}/robot/{robot_id}/grab"
+    response = await client.post(url, timeout=5.0)
+    data = response.json()
+    return f"[{data['part']}]: part"
     # TODO: เติมโค้ดส่ง POST request ไปยัง /student/{STUDENT_ID}/robot/{robot_id}/grab
     # พร้อมแนบ JSON Payload {"part": part}
-    pass
 
 async def run_robot_task(client: httpx.AsyncClient, robot_id: str):
     """สั่งให้หุ่นยนต์ 1 ตัว ทำการหยิบชิ้นส่วน A, B, และ C ตามลำดับ"""
+    
     # TODO: วนลูปหยิบชิ้นส่วนใน PARTS ตามลำดับเรียงกัน (Sequential inside single robot)
     pass
 
@@ -40,6 +48,13 @@ async def main():
         
         start_time = time.time()
         print("Starting Async Robot Operation...")
+
+        result = asyncio.gather (
+            run_robot_task(robot_1),
+            run_robot_task(robot_2),
+            run_robot_task(robot_3),
+            run_robot_task(robot_4)
+        )
         
         # TODO: สั่งรัน run_robot_task ของหุ่นยนต์ทั้ง 4 ตัวพร้อมกันโดยใช้ asyncio.gather
         
